d90b16cff06c9da3b1c310565153c33ca5c41e9a
aoners <aoners@gmail.com>
2019-01-08 15:11:20 +0800

    delete deps

--- deps/mariaex/lib/mariaex/geometry/line_string.ex
+++
@@ -1,8 +0,0 @@
-defmodule Mariaex.Geometry.LineString do
-  @moduledoc """
-  Define the LineString struct
-  """
-
-  @type t :: %Mariaex.Geometry.LineString{ coordinates: [{number, number}], srid: non_neg_integer | nil }
-  defstruct coordinates: [], srid: nil
-end
--- deps/mariaex/lib/mariaex/geometry/polygon.ex
+++
@@ -1,8 +0,0 @@
-defmodule Mariaex.Geometry.Polygon do
-  @moduledoc """
-  Define the Polygon struct
-  """
-
-  @type t :: %Mariaex.Geometry.Polygon{ coordinates: [[{number, number}]], srid: non_neg_integer | nil }
-  defstruct coordinates: [], srid: nil
-end
